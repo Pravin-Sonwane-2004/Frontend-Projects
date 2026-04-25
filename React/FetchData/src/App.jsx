d4405abb88a47da5aba70b81af905d394f2fae44
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-04-25 17:16:19 +0530

    this is todays work

--- React/FetchData/src/App.jsx
+++ React/FetchData/src/App.jsx
@@ -29,23 +29,91 @@
 
 //now upgraded version
 
-export default function App() {
-    const user = {
-      name: "Pravin",
-      skill: "Java + Spring Boot",
-      learning: "React",
-      goal: "Full Stack Developer"
-    }
-    return (
-      <>
-      <h1>Introduce</h1>
-      <ul style={{listStyle : "none"}}>
-        <li>{user.name}</li>
-         <li>{user.skill}</li>
-          <li>{user.learning}</li>
-           <li>{user.goal}</li>
-      </ul>
-      </>
-    )
+// export default function App() {
+//     const user = {
+//       name: "Pravin",
+//       skill: "Java + Spring Boot",
+//       learning: "React",
+//       goal: "Full Stack Developer"
+//     }
+//     return (
+//       <>
+//       <h1>Introduce</h1>
+//       <ul style={{listStyle : "none"}}>
+//         <li>{user.name}</li>
+//          <li>{user.skill}</li>
+//           <li>{user.learning}</li>
+//            <li>{user.goal}</li>
+//       </ul>
+//       </>
+//     )
   
+// }
+
+// import { useState } from "react";
+
+// export default function App() {
+// const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//     <h1>Counter Applicarion</h1>
+//     <h2>Count : {count} </h2>
+//     {/* <button onClick={() => setCount(count +1)}>increse</button>
+//     <button onClick={() => setCount(count -1)}>decrese</button> */}
+
+//       {/* or in other words to prevent bugs when we use sync */}
+//       <button onClick={() => setCount(count +1)}>increse</button>
+//     <button onClick={() => setCount(count -1)}>decrese</button>
+//     </>
+//   )
+// }
+
+//now its time to work with from 
+
+import { useState } from "react";
+
+export default function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: ""
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+  }
+
+  return (
+    <>
+      <h1>User Form</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
