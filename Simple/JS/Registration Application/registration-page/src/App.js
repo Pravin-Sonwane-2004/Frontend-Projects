05bd32bc60af0214147c6cee0a4080f4ec62c95f
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-05-23 00:50:25 +0530

    frontend is frustrated

--- Simple/JS/Registration Application/registration-page/src/App.js
+++ Simple/JS/Registration Application/registration-page/src/App.js
@@ -1,21 +1,67 @@
+import { useState } from "react";
+import axios from "axios";
 
+export default function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: ""
+  });
 
-function App() {
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  async function sendData(event) {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/createuser",
+        formData
+      );
+
+      alert("Data sent successfully");
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error sending data:", error);
+      alert("Failed to create user.");
+    }
+  }
+
   return (
     <>
-        <h1 style={{}}> hello </h1>
-    <div>
-    <label>
-    Name:
-    <input type="text" name="username" autocomplete="name" />
-  </label>
-  <label>
-    Email:
-    <input type="email"/>
-  </label>
-    </div>
+      <h1>Hello</h1>
+
+      <form className="parent" onSubmit={sendData}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            autoComplete="name"
+          />
+        </label>
+
+        <label>
+          Pass:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            autoComplete="current-password"
+          />
+        </label>
+
+        <button type="submit">Create User</button>
+      </form>
     </>
   );
 }
-
-export default App;
