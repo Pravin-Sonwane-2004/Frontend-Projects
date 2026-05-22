7fbdb03ffc77474ce88710b347f6768036f03958
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-05-22 17:18:27 +0530

    added new project

---
+++ Simple/JS/Registration Application/registration-page/src/App.js
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <>
+        <h1 style={{}}> hello </h1>
+    <div>
+    <label>
+    Name:
+    <input type="text" name="username" autocomplete="name" />
+  </label>
+  <label>
+    Email:
+    <input type="email"/>
+  </label>
+    </div>
+    </>
+  );
+}
+
+export default App;
